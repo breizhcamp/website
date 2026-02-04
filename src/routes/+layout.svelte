fcf17ffd6c9c97ac8111b45ebe34bf8b8d28b7b7
Johanna DUIGOU <jduigou.dev@gmail.com>
2026-02-04 22:18:00 +0100

    ✨ feat(layout): layout global

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-	import '../lib/css/app.css';
+	import '../lib/css/index.css';
 	import Header from '../lib/components/layout/Header.svelte';
 	import Footer from '../lib/components/layout/Footer.svelte';
 </script>
